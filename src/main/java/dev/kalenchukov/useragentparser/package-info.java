08ff0184d885f4ee525605b4c86041e6e31e767d
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-03-20 12:19:03 +0300

    Обновлёна версия

--- src/main/java/dev/kalenchukov/useragentparser/package-info.java
+++ src/main/java/dev/kalenchukov/useragentparser/package-info.java
@@ -4,6 +4,6 @@
  * @author <a href="mailto:aleksey.kalenchukov@yandex.ru">Алексей Каленчуков</a>
  * @copyright © 2021 Алексей Каленчуков
  * @package UserAgentParser
- * @version 1.0.0
+ * @version 1.1.0
  */
 package dev.kalenchukov.useragentparser;

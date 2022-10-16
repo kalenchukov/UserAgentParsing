6d3f7f5bc870cc75b179cd884bd35f9d70a2691d
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2022-10-16 17:01:31 +0300

    Добавлены комментарии для всех типов перечислений BrowserType

--- src/main/java/dev/kalenchukov/useragent/parsing/resources/BrowserType.java
+++ src/main/java/dev/kalenchukov/useragent/parsing/resources/BrowserType.java
@@ -31,310 +31,541 @@ import org.jetbrains.annotations.Nullable;
  */
 public enum BrowserType
 {
+	/**
+	 * Не определён.
+	 */
 	UNKNOWN(
 		null
 	),
 
+	/**
+	 * Google Chrome.
+	 */
 	GOOGLE_CHROME(
 		"Google Chrome"
 	),
 
+	/**
+	 * Opera.
+	 */
 	OPERA(
 		"Opera"
 	),
 
+	/**
+	 * Firefox.
+	 */
 	FIREFOX(
 		"Firefox"
 	),
 
+	/**
+	 * Internet Explorer.
+	 */
 	INTERNET_EXPLORER(
 		"Internet Explorer"
 	),
 
+	/**
+	 * Yandex.
+	 */
 	YANDEX_BROWSER(
 		"Yandex browser"
 	),
 
+	/**
+	 * Edge.
+	 */
 	EDGE(
 		"Edge"
 	),
 
+	/**
+	 * Safari.
+	 */
 	SAFARI(
 		"Safari"
 	),
 
+	/**
+	 * Chromium.
+	 */
 	CHROMIUM(
 		"Chromium"
 	),
 
+	/**
+	 * Otter.
+	 */
 	OTTER_BROWSER(
 		"Otter Browser"
 	),
 
+	/**
+	 * Vivaldi.
+	 */
 	VIVALDI(
 		"Vivaldi"
 	),
 
+	/**
+	 * Maxthon.
+	 */
 	MAXTHON(
 		"Maxthon"
 	),
 
+	/**
+	 * Konqueror.
+	 */
 	KONQUEROR(
 		"Konqueror"
 	),
 
+	/**
+	 * SRWare Iron.
+	 */
 	SRWARE_IRON(
 		"SRWare Iron"
 	),
 
+	/**
+	 * SeaMonkey.
+	 */
 	SEAMONKEY(
 		"SeaMonkey"
 	),
 
+	/**
+	 * Aol.
+	 */
 	AOL_BROWSER(
 		"Aol Browser"
 	),
 
+	/**
+	 * IceWeasel.
+	 */
 	ICEWEASEL(
 		"IceWeasel"
 	),
 
+	/**
+	 * Epiphany.
+	 */
 	EPIPHANY(
 		"Epiphany"
 	),
 
+	/**
+	 * Kazehakase.
+	 */
 	KAZEHAKASE(
 		"Kazehakase"
 	),
 
+	/**
+	 * Galeon.
+	 */
 	GALEON(
 		"Galeon"
 	),
 
+	/**
+	 * IceCat.
+	 */
 	ICECAT(
 		"IceCat"
 	),
 
+	/**
+	 * Opera Touch.
+	 */
 	OPERA_TOUCH(
 		"Opera Touch"
 	),
 
+	/**
+	 * Flock.
+	 */
 	FLOCK(
 		"Flock"
 	),
 
+	/**
+	 * Netscape Navigator.
+	 */
 	NETSCAPE_NAVIGATOR(
 		"Netscape Navigator"
 	),
 
+	/**
+	 * Phoenix.
+	 */
 	PHOENIX_BROWSER(
 		"Phoenix Browser"
 	),
 
+	/**
+	 * UCBrowser.
+	 */
 	UCBROWSER(
 		"UCBrowser"
 	),
 
+	/**
+	 * Arora.
+	 */
 	ARORA(
 		"Arora"
 	),
 
+	/**
+	 * Minimo.
+	 */
 	MINIMO(
 		"Minimo"
 	),
 
+	/**
+	 * OmniWeb.
+	 */
 	OMNIWEB(
 		"OmniWeb"
 	),
 
+	/**
+	 * Shiira.
+	 */
 	SHIIRA(
 		"Shiira"
 	),
 
+	/**
+	 * Sunrise.
+	 */
 	SUNRISE_BROWSER(
 		"Sunrise Browser"
 	),
 
+	/**
+	 * Nokia.
+	 */
 	NOKIA_BROWSER(
 		"Nokia Browser"
 	),
 
+	/**
+	 * Camino.
+	 */
 	CAMINO(
 		"Camino"
 	),
 
+	/**
+	 * Chimera.
+	 */
 	CHIMERA(
 		"Chimera"
 	),
 
+	/**
+	 * IceApe.
+	 */
 	ICEAPE(
 		"IceApe"
 	),
 
+	/**
+	 * Midori.
+	 */
 	MIDORI(
 		"Midori"
 	),
 
+	/**
+	 * LeechCraft.
+	 */
 	LEECHCRAFT(
 		"LeechCraft"
 	),
 
+	/**
+	 * Miui.
+	 */
 	MIUI_BROWSER(
 		"Miui Browser"
 	),
 
+	/**
+	 * Baidu HD.
+	 */
 	BAIDU_BROWSER_HD(
 		"Baidu Browser HD"
 	),
 
+	/**
+	 * K-Ninja.
+	 */
 	K_NINJA(
 		"K-Ninja"
 	),
 
+	/**
+	 * NetFront.
+	 */
 	NETFRONT(
 		"NetFront"
 	),
 
+	/**
+	 * ThunderBrowse.
+	 */
 	THUNDERBROWSE(
 		"ThunderBrowse"
 	),
 
+	/**
+	 * Pale Moon.
+	 */
 	PALE_MOON(
 		"Pale Moon"
 	),
 
+	/**
+	 * Aloha.
+	 */
 	ALOHA_BROWSER(
 		"Aloha Browser"
 	),
 
+	/**
+	 * Samsung.
+	 */
 	SAMSUNG_BROWSER(
 		"Samsung Browser"
 	),
 
+	/**
+	 * Dolphin.
+	 */
 	DOLPHIN_BROWSER(
 		"Dolphin Browser"
 	),
 
+	/**
+	 * Mercury.
+	 */
 	MERCURY(
 		"Mercury"
 	),
 
+	/**
+	 * K-Meleon.
+	 */
 	K_MELEON(
 		"K-Meleon"
 	),
 
+	/**
+	 * QQBrowser.
+	 */
 	QQBROWSER(
 		"QQBrowser"
 	),
 
+	/**
+	 * Opera Mini.
+	 */
 	OPERA_MINI(
 		"Opera Mini"
 	),
 
+	/**
+	 * Opera Mobi.
+	 */
 	OPERA_MOBI(
 		"Opera Mobi"
 	),
 
+	/**
+	 * Sputnik.
+	 */
 	SPUTNIK_BROWSER(
 		"Sputnik Browser"
 	),
 
+	/**
+	 * Bing Search App.
+	 */
 	BING_SEARCH_APP(
 		"Bing Search App"
 	),
 
+	/**
+	 * Nintendo DS Browser.
+	 */
 	NINTENDO_BROWSER(
 		"NintendoBrowser"
 	),
 
+	/**
+	 * Sleipnir.
+	 */
 	SLEIPNIR(
 		"Sleipnir"
 	),
 
+	/**
+	 * Crazy Browser.
+	 */
 	CRAZY_BROWSER(
 		"Crazy Browser"
 	),
 
+	/**
+	 * Coc Coc Browser.
+	 */
 	COC_COC_BROWSER(
 		"Coc Coc Browser"
 	),
 
+	/**
+	 * LG Browser.
+	 */
 	LG_BROWSER(
 		"LG Browser"
 	),
 
+	/**
+	 * Quark Browser
+	 */
 	QUARK(
 		"Quark Browser"
 	),
 
+	/**
+	 * Surf.
+	 */
 	SURF(
 		"Surf"
 	),
 
+	/**
+	 * Huawei Browser.
+	 */
 	HUAWEIBROWSER(
 		"Huawei Browser"
 	),
 
+	/**
+	 * Elements Browser.
+	 */
 	ELEMENTS_BROWSER(
 		"Elements Browser"
 	),
 
+	/**
+	 * Naver Whale.
+	 */
 	NAVER_WHALE(
 			"Naver Whale"
 	),
 
+	/**
+	 * Amigo.
+	 */
 	AMIGO(
 		"Amigo"
 	),
 
+	/**
+	 * Meizu Browser.
+	 */
 	MZBROWSER(
 		"Meizu Browser"
 	),
 
+	/**
+	 * Stainless.
+	 */
 	STAINLESS(
 		"Stainless"
 	),
 
+	/**
+	 * OviBrowser.
+	 */
 	OVIBROWSER(
 		"OviBrowser"
 	),
 
+	/**
+	 * Baidu.
+	 */
 	BAIDU_BROWSER(
 		"Baidu"
 	),
 
+	/**
+	 * Puffin Browser.
+	 */
 	PUFFIN(
 		"Puffin Browser"
 	),
 
+	/**
+	 * Minefield Browser.
+	 */
 	MINEFIELD(
 		"Minefield Browser"
 	),
 
+	/**
+	 * Shiretoko Browser.
+	 */
 	SHIRETOKO(
 		"Shiretoko Browser"
 	),
 
+	/**
+	 * Namoroka Browser.
+	 */
 	NAMOROKA(
 		"Namoroka Browser"
 	),
 
+	/**
+	 * Vivo Browser.
+	 */
 	VIVOBROWSER(
 		"VivoBrowser"
 	),
 
+	/**
+	 * HeyTapBrowser.
+	 */
 	HEYTAPBROWSER(
 		"HeyTapBrowser"
 	),
 
+	/**
+	 * OppoBrowser.
+	 */
 	OPPOBROWSER(
 		"OppoBrowser"
 	),
 
+	/**
+	 * Superbird.
+	 */
 	SUPERBIRD(
 		"Superbird"
 	),
 
+	/**
+	 * Lunascape.
+	 */
 	LUNASCAPE(
 		"Lunascape"
 	);

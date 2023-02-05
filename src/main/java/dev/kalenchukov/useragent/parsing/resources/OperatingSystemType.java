746fdca62917a848ebbedf05428b38c799b3db94
Aleksey Kalenchukov <aleksey.kalenchukov@yandex.ru>
2023-02-05 19:31:00 +0300

    Обновлён копирайт

--- src/main/java/dev/kalenchukov/useragent/parsing/resources/OperatingSystemType.java
+++ src/main/java/dev/kalenchukov/useragent/parsing/resources/OperatingSystemType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Алексей Каленчуков
+ * Copyright © 2022-2023 Алексей Каленчуков
  * GitHub: https://github.com/kalenchukov
  * E-mail: mailto:aleksey.kalenchukov@yandex.ru
  *
